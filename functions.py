690fd83dadc1d0570b416660ff65e529ef758bce
Dimitrije Dolga <ddimitrije@gmail.com>
2022-11-23 14:01:38 +0100

    first commit

---
+++ functions.py
@@ -0,0 +1,18 @@
+FILEPATH = "todos_item.txt"
+
+
+def get_todos(filepath = FILEPATH):
+
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos(todo_arg, filepath = FILEPATH):
+    with open(filepath, 'w') as file:
+        file.writelines(todo_arg)
+
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
